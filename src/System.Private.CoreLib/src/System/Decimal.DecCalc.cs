c387ebcde7e1b7e7d5741ff2dadcf07de51d60ea
EgorBo <egorbo@gmail.com>
2018-06-28 01:11:03 +0300

    mcs workaround for Decimal

--- src/System.Private.CoreLib/src/System/Decimal.DecCalc.cs
+++ src/System.Private.CoreLib/src/System/Decimal.DecCalc.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
+#if !__MonoCS__
 using System.Diagnostics;
 using System.Diagnostics.Private;
 using System.Runtime.CompilerServices;
@@ -2675,3 +2675,4 @@ done:
         }
     }
 }
+#endif
